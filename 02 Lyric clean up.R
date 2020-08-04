777e61953533f3bf959dc3bde81d1ffe1060a029
Insha <38670082+insha-chaddha@users.noreply.github.com>
2020-08-04 19:03:36 -0400

    Update 02 Lyric clean up.R

--- 02 Lyric clean up.R
+++ 02 Lyric clean up.R
@@ -1,5 +1,6 @@
 library(stringr)
 library(tidyverse)
+library(quanteda)
 
 ##lyric data
 
@@ -11,25 +12,49 @@ song_df_2 <- song_df %>%
   mutate(lyrics_clean= gsub("\\[[^][]*]","",lyrics)) %>% ##AZ lyrics adds comments in square brackets
   mutate(lyrics_clean= gsub("\n|\\s{2,}"," ",lyrics_clean)) %>% ##tidying up spaces
   mutate(lyrics_clean= gsub("\"|\\\\","",lyrics_clean)) %>%
-  mutate(No_Wrds= sapply(strsplit(lyrics_clean, "\\s"), length)) 
-  
+  mutate(No_Wrds= sapply(strsplit(lyrics_clean, "\\s"), length))
+
 
 lyrics_list=list()
 for (i in song_df_2$ID) {
+  lyric <- unlist((str_split(song_df_2[i, "lyrics_clean"], "\\s")))
   
-  x <- as.data.frame(str_split(song_df_2[i, "lyrics_clean"], "\\s")) %>% 
+  x <- data.frame("lyrics"=lyric)%>% 
     mutate(artist= song_df_2[i, "artist"]) %>% 
-    mutate(songname=song_df_2[i, "songname"])
+    mutate(songname=song_df_2[i, "songname"])  %>% 
+    mutate(ID=song_df_2[i, "ID"] ) %>% 
+    mutate(rank=song_df_2[i, "rank"] ) %>% 
+    mutate(year=song_df_2[i, "year"] ) %>% 
+    mutate_all(as.character)  %>% 
+    filter(grepl("\\s|...|-", lyrics))
+  
 
   lyrics_list[[paste0(song_df_2[i,"songname"], "_", song_df_2[i,"artist"] )]] <- x
   
 }
 
-all_words <- do.call("rbind", lyrics_list)
+all_words <- do.call("rbind", lyrics_list) 
 
-names(all_words) <- c("lyrics", "artist", "songname") 
 
-all_words %>% 
-  group_by(artist, songname ) %>% 
+
+all_words_complete <- all_words %>% 
+  group_by(artist, songname , ID, rank, year) %>% 
+  mutate(syllables=nsyllable(lyrics)) %>% 
+  mutate(three_more_syll =ifelse(syllables>=3, 1, 0)) 
+
+
+summary1 <- all_words2%>% 
   summarize(total_words= n(),
-    unique_words= n_distinct(lyrics))
+    unique_words= n_distinct(lyrics) , 
+    avg_syllables= mean(syllables, na.rm=T),
+    no_words_with_three_or_mor= sum(three_more_syll, na.rm=T)) 
+
+all_words_unique <- all_words2 %>% 
+  distinct()
+
+
+summary2 <- all_words_unique%>% 
+  summarize(
+            no_words_with_three_or_mor= sum(three_more_syll, na.rm=T)) 
+
+saveRDS(all_words_complete, "all_words_complete.rds")
